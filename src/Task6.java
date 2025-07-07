7cb0955e6d15154365eba16a24c7d49acdd07839
Azerty <nik.k.2018@mail.ru>
2025-07-07 18:06:57 +0300

    Задача 6: Вывод последовательности кратной 7

---
+++ src/Task6.java
@@ -0,0 +1,8 @@
+public class Task6 {
+    public static void main(String[] args) {
+        // Цикл от 7 до 98 с шагом 7
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.print(i + " ");
+        }
+    }
+}
