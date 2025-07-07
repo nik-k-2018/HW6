6be67d0677f9542ef18313300d01ccc7ce819c68
Azerty <nik.k.2018@mail.ru>
2025-07-07 18:00:57 +0300

    Задача 3: Вывод четных чисел от 0 до 17

---
+++ src/Task3.java
@@ -0,0 +1,11 @@
+public class Task3 {
+    public static void main(String[] args) {
+        // Цикл от 0 до 17
+        for (int i = 0; i <= 17; i++) {
+            // Проверяем, является ли число чётным
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
