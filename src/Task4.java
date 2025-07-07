fedcb39dbf14cd9c7e2edcfaf626af5d1dcf7f0e
Azerty <nik.k.2018@mail.ru>
2025-07-07 18:02:50 +0300

    Задача 4: Вывод чисел от 10 до -10

---
+++ src/Task4.java
@@ -0,0 +1,8 @@
+public class Task4 {
+    public static void main(String[] args) {
+        // Цикл от 10 до -10
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+}
